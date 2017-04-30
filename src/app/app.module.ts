6cfb622c08a9a0c350a9568fa3e4a4ee26e59288
Grace Liao <lcihaeon@gmail.com>
2017-04-30 16:32:41 -0700

    import necessary material modules and libraries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import {FlexLayoutModule} from "@angular/flex-layout";
 import {NoopAnimationsModule} from "@angular/platform-browser/animations";
-import {MdCheckboxModule, MdButtonModule, MdInputModule} from "@angular/material";
+import {
+  MdCheckboxModule, MdButtonModule, MdInputModule, MdAutocompleteModule, MdSliderModule,
+  MdButtonToggleModule, MdRadioModule, MdIconModule, MdSlideToggleModule
+} from "@angular/material";
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     NoopAnimationsModule,
     MdButtonModule,
     MdCheckboxModule,
     MdInputModule,
+    MdAutocompleteModule,
+    MdSliderModule,
+    MdSlideToggleModule,
+    MdButtonToggleModule,
+    MdRadioModule,
+    MdIconModule,
     FlexLayoutModule
   ],
   providers: [],
